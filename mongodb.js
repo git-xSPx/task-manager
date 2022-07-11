e2088f119f15833c43930c92afc7443bc29e3229
Sergey Pidoprigora <work.xspx@gmail.com>
2022-07-12 00:02:07 +0300

    part 82

--- mongodb.js
+++ mongodb.js
@@ -11,7 +11,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
+    
+    // create
+    /*
+    db.collection('users').insertOne({
+        name: "Serhii",
+        age: 25
+    }).then((result) => {
+        console.log(result.insertedId)
+    }).catch((error) => {
+        console.log(error)
+    })
+    */
 
+    // read
     /*
     db.collection('users').findOne({ _id: new ObjectId('62caacc6f529f809ab458fc9') }, (error, user) => {
         if (error) {
@@ -19,8 +32,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         }
         console.log(user)
     })
-    */
-    /*
+    
+    
     db.collection('users').find({ age: 25 }).toArray((error, users) => {
         console.log(users)
     })
@@ -28,7 +41,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     db.collection('users').find({ age: 25 }).count((error, count) => {
         console.log(count)
     })
-    */
+    
+    
     db.collection('tasks').findOne({ _id: new ObjectId('62b4d7e1a8d51556e93ee301') }, (error, user) => {
         if (error) {
             console.log('Unable to fetch!')
@@ -41,4 +55,55 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         }
         console.log(tasks)
     })
+    */
+    
+    // update
+    /*
+    db.collection('users').updateOne(
+        {
+            _id : ObjectId("62cc8097886dd31939de60ae")
+        },
+        {
+            $inc: { age: 3 }
+        }
+    ).then((result) => {
+        console.log(result)
+    }).catch ((error) => {
+        console.log(error)
+    })
+    
+    db.collection('tasks').updateMany(
+        {
+            completed : false
+        },
+        {
+            $set: { completed : true }
+        }
+    ).then((result) => {
+        console.log(result)
+    }).catch ((error) => {
+        console.log(error)
+    })
+    */
+
+    //delete
+    /*
+    db.collection('users').deleteMany(
+        {
+            age : 28
+        }
+    ).then((result) => {
+        console.log(result)
+    }).catch ((error) => {
+        console.log(error)
+    })
+    */
+   db.collection('tasks').deleteOne({
+        description: 'Check result'
+   }).then((result) => {
+        console.log(result)
+   }).catch((error) => {
+        console.log(error)
+   })
+   
 })
